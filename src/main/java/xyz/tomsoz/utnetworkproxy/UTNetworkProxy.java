e9f2561f46c45740a7287294a7bd9f3c745cbfa1
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 15:56:33 +0100

    Plugin initialisation

--- src/main/java/xyz/tomsoz/utnetworkproxy/UTNetworkProxy.java
+++ src/main/java/xyz/tomsoz/utnetworkproxy/UTNetworkProxy.java
@@ -2,16 +2,17 @@ package xyz.tomsoz.utnetworkproxy;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
+import xyz.tomsoz.utnetworkproxy.Misc.Utils;
 
 public final class UTNetworkProxy extends Plugin {
 
     @Override
     public void onEnable() {
-        ProxyServer.getInstance().getConsole().sendMessage();
+        ProxyServer.getInstance().getConsole().sendMessage(Utils.chat("&aThe plugin has enabled successfully."));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        ProxyServer.getInstance().getConsole().sendMessage(Utils.chat("&cThe plugin has disabled successfully."));
     }
 }
