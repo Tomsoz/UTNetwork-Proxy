001e95de8ba5a4979b1e151a5f9bfe8e06157276
BuildTools <unconfigured@null.spigotmc.org>
2022-08-02 23:17:49 +0100

    Made username colour update at ChatEvent too.

--- src/main/java/tk/utnetwork/utnetworkproxy/Events/ChatEvent.java
+++ src/main/java/tk/utnetwork/utnetworkproxy/Events/ChatEvent.java
@@ -1,5 +1,9 @@
 package tk.utnetwork.utnetworkproxy.Events;
 
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.User;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -16,6 +20,12 @@ public class ChatEvent implements Listener {
     public void onChat(net.md_5.bungee.api.event.ChatEvent e) {
         if (!(e.getSender() instanceof ProxiedPlayer) || e.isCommand() || e.isProxyCommand()) return;
         ProxiedPlayer p = (ProxiedPlayer) e.getSender();
+
+        LuckPerms api = LuckPermsProvider.get();
+        User u = api.getUserManager().getUser(p.getUniqueId());
+        String colour = api.getGroupManager().getGroup(u.getPrimaryGroup()).getCachedData().getMetaData().getPrefixes().get(0);
+
+        p.setDisplayName(ChatColor.translateAlternateColorCodes('&', colour + p.getName()));
 
         if (e.getMessage().startsWith("a# ")) {
             String msg = e.getMessage().replaceFirst("a# ", "");
