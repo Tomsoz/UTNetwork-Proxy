76db36b08466e22d4b8cdf13c3c3b97d1efde4d7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-07 05:08:10 +0100

    Added very broken plugin messaging ill fix in morning

--- src/main/java/tk/utnetwork/utnetworkproxy/Events/PluginMessage.java
+++ src/main/java/tk/utnetwork/utnetworkproxy/Events/PluginMessage.java
@@ -1,7 +1,10 @@
 package tk.utnetwork.utnetworkproxy.Events;
 
 import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -16,22 +19,45 @@ public class PluginMessage implements Listener {
 
     @EventHandler
     public void onPluginMessage(PluginMessageEvent e) {
-        if (!e.getTag().equalsIgnoreCase("spigot:messaging")) {
-            return;
-        }
         ByteArrayDataInput in = ByteStreams.newDataInput(e.getData());
         String subChannel = in.readUTF();
         if (subChannel.equalsIgnoreCase("CommandAsPlayer" )) {
             if (e.getReceiver() instanceof ProxiedPlayer) {
                 ProxiedPlayer receiver = (ProxiedPlayer) e.getReceiver();
                 String cmd = in.readUTF();
-                receiver.chat("/" + cmd);
+                ProxyServer.getInstance().getPluginManager().dispatchCommand(receiver, cmd);
             }
-//            the receiver is a server when the proxy talks to a server
-//            if (e.getReceiver() instanceof Server) {
-//                Server receiver = (Server) e.getReceiver();
-//                do things
-//            }
+        }
+
+        if (subChannel.equalsIgnoreCase("MaxPlayerCountRequest" )) {
+            if (e.getReceiver() instanceof ProxiedPlayer) {
+                ProxiedPlayer receiver = (ProxiedPlayer) e.getReceiver();
+                String server = in.readUTF();
+                ServerInfo s = ProxyServer.getInstance().getServerInfo(server);
+                if (s == null) return;
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("MaxPlayerCountRequest");
+                out.writeUTF(s.getName());
+                out.writeUTF(receiver.getServer().getInfo().getName());
+
+                s.sendData("spigot:messaging", out.toByteArray());
+            }
+        }
+
+        if (subChannel.equalsIgnoreCase("MaxPlayerCountResponse" )) {
+            if (e.getReceiver() instanceof ProxiedPlayer) {
+                ProxiedPlayer receiver = (ProxiedPlayer) e.getReceiver();
+                String servers = in.readUTF();
+                String[] serversList = servers.split("_____");
+                ServerInfo from = ProxyServer.getInstance().getServerInfo(serversList[0]);
+                ServerInfo to = ProxyServer.getInstance().getServerInfo(serversList[1]);
+                if (from == null || to == null) return;
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("MaxPlayerCountResponse");
+                out.writeUTF(from.getName());
+
+                to.sendData("spigot:messaging", out.toByteArray());
+            }
         }
     }
 }
